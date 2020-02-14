fee521371a5a3ae476a27f6e7738fa811764a2c6
HansLemurson <lemurman384@gmail.com>
2020-02-14 14:45:16 -0800

    Changing Click behavior
    Preparing to make clicks be tile-based rather than based on coordinates.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #########1#########2#########3#########4#########5#########6#########7#########8
 from processing import *
 import random
-import terrain
+import terrains
 import units
-####################### Globals #######################################
+####################### Globals ################################################
 NEIGHBOR_OFFSETS = (
   (-1,-1),(-1,0),(-1,1),
   (0,-1),       (0,1),
@@ -15,40 +15,41 @@ DIRT = '#654321'
 FOREST = '#339966'
 MOUNTAIN = '#778899'
 
-####################### Classes #######################################
+####################### Classes ################################################
 
-##### Tile Class #####
+##### Tile Class ===============================================================
 class Tile:
   terrain_colors = {"ocean":WATER,"land":DIRT,"grassland":GRASS,
       "forest":FOREST,"mountain":MOUNTAIN}
-  #------------------------------------------------------  
+  #-----------------------------------------------------------------------------
   def __init__(self,grid,terrain_type,col,row):
     self.col = col
     self.row = row
     self.grid = grid
-    self.terrain = terrain.getTerrain(terrain_type)
-    self.color = Tile.terrain_colors[terrain_type]
+    self.terrain = terrains.getTerrain(terrain_type)
+    self.color = self.terrain["color"]
     self.units = []
     self.city = None
-  #----------------------------------------------------------
+  #-----------------------------------------------------------------------------
   def __str__(self):
     return "<{} @(c:{},r:{})>".format(self.terrain,self.col,self.row)
-  #--------------------
+  #-----------------------------------------------------------------------------
   def __repr__(self):
     return str(self)
-  #---------------------------------------------------------
+  #-----------------------------------------------------------------------------
+  #-----------------------------------------------------------------------------
   def getCorner(self):
     size = self.grid.size
     x = self.col*size + self.grid.x_corner
     y = self.row*size + self.grid.y_corner
     return x,y
-  #------------------------------------------------------  
+  #-----------------------------------------------------------------------------
   def getCenter(self):
     size = self.grid.size
     x = (self.col+0.5)*size + self.grid.x_corner
     y = (self.row+0.5)*size + self.grid.y_corner
     return x,y
-  #------------------------------------------------------
+  #-----------------------------------------------------------------------------
   def draw(self):
     '''Tile Draws itself and any contents.'''
     stroke(0,153)
@@ -57,31 +58,38 @@ class Tile:
     size = self.grid.size
     x,y = self.getCorner()
     rect(x,y,size,size)
-    
+    # Draw Cities
     if self.city:
       self.city.draw()
-    
+    # Draw Units
     if self.units:
-      for unit in self.units:
-        unit.draw()
-  #------------------------------------------------------
+      self.units[-1].draw() #Draws "Top" unit
+      #Add indicator for multiple units
+      if len(self.units)>1:
+        textSize(0.6*size)
+        text("+",x+0.2*size,y+0.2*size)
+  #-----------------------------------------------------------------------------
 
-##### Grid Class ===============================================
+##### Grid Class ===============================================================
 #The Grid holds an array of tiles and handles their interactions.
 class Grid:
   """The Grid holds an array of Tiles and handles their interactions."""
-  #----------------------------------------------------------
+  #-----------------------------------------------------------------------------
   def __init__(self,columns,rows,size=40,x_corner=20,y_corner=20):
     self.x_corner = x_corner
     self.y_corner = y_corner
     self.cols = columns
     self.rows = rows
     self.selected_coords = None
+    self.selected_tile = None
     self.target_coords = None
+    self.target_tile = None
     self.size = size #The size of one tile (assume a square)
     self.tiles = self.makeMap(columns,rows) # Array of Tiles
     self.units = [] # List of units on map
-  #----------------------------------------------------------
+    self.selected_unit = None # The unit currently selected
+    self.cities = []
+  #-----------------------------------------------------------------------------
   # Generates a map as a Tile array and returns it
   def makeMap(self,cols,rows):
     lands = ["grassland","forest","mountain"]
@@ -108,33 +116,42 @@ class Grid:
         new_row.append(Tile(self,new_terrain,c,r))
       new_map.append(new_row)
     return new_map
-  #----------------------------------------------------------
+  #-----------------------------------------------------------------------------
   def addUnit(self,unit_type,col,row):
     """Adds a new unit to the grid at specified location.  Assigns to proper lists."""
     target_coords = self.tiles[row][col]
     unit = units.Unit(target_coords,unit_type)
     target_coords.units.append(unit)
     self.units.append(unit)
-  #----------------------------------------------------------
+  #-----------------------------------------------------------------------------
   def tryToMoveUnitTo(self,target_tile):
-    '''If there is a unit to move, and a valid place to move it to, do so.'''
+    '''If there is a selected unit, and a valid place to move it to, do so.'''
     pass
-  #----------------------------------------------------------
+  #-----------------------------------------------------------------------------
   def hasMouse(self):
     return (0 < mouseX-self.x_corner < self.cols*self.size) and (0 < mouseY-self.y_corner < self.rows*self.size)
-  #----------------------------------------------------------
+  #-----------------------------------------------------------------------------
+  def inGrid(self,col,row):
+    '''Returns whether a given column and row are actually within the Grid'''
+    return 0<=row<self.rows and 0<=col<self.cols
+  #-----------------------------------------------------------------------------
   def squareWithMouse(self):
     col = (mouseX-self.x_corner)//self.size
     row = (mouseY-self.y_corner)//self.size
     return (col,row)
-  #----------------------------------------------------------
-  def inGrid(self,col,row):
-    return 0<=row<self.rows and 0<=col<self.cols
-  #----------------------------------------------------------
+  #-----------------------------------------------------------------------------
+  def tileWithMouse(self):
+    col = (mouseX-self.x_corner)//self.size
+    row = (mouseY-self.y_corner)//self.size
+    if self.inGrid(col,row):
+      return self.tiles[row][col]
+    else:
+      return None
+  #-----------------------------------------------------------------------------
   def getTile(self,coords):
     col,row = coords
     return self.tiles[row][col]
-  #----------------------------------------------------------  
+  #-----------------------------------------------------------------------------
   def tileNeighbors(self,col,row):
     neighbors = []
     for dr,dc in NEIGHBOR_OFFSETS:
@@ -144,16 +161,18 @@ class Grid:
     # #List comprehension version
     # return [self.tiles[row+dr][col+dc] for dr,dc in NEIGHBOR_OFFSETS if self.inGrid(col+dc,row+dr)]
       
-  #----------------------------------------------------------
+  #-----------------------------------------------------------------------------
   def handleClick(self):
     mcol,mrow = self.squareWithMouse()
     if self.inGrid(mcol,mrow):
-      # Handle Left-Clicks
+      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      # Handle Left-Clicks 
       if mouseButton == LEFT:
         if self.selected_coords == (mcol,mrow):
           self.selected_coords = None
         else:
           self.selected_coords = (mcol,mrow)
+      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       # Handle Right-Clicks
       elif mouseButton == RIGHT:
         self.target_coords = (mcol,mrow)
@@ -162,12 +181,13 @@ class Grid:
         if self.selected_coords: #If a tile is even selected
           selected_tile = self.getTile(self.selected_coords)
           if selected_tile.units: #If that tile has any units
-            active_unit = selected_tile.units[0] #Take first unit in list
+            active_unit = selected_tile.units[-1] #Take last unit in list
             if active_unit.canMoveTo(target_tile): #Check if tile can be moved to
               active_unit.move(target_tile) # Move it there.
               self.selected_coords = self.target_coords #Bring selection along too
               self.target_coords = None # Clear the target
-  #----------------------------------------------------------
+      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #-----------------------------------------------------------------------------
   # Draws the whole Grid to the screen
   def draw(self):
     stroke(0)
@@ -175,6 +195,10 @@ class Grid:
     for r, row in enumerate(self.tiles):
       for c, tile in enumerate(row):
         tile.draw() # Tile draws itself!
+        
+    # for unit in self.units: #Units instead will be drawn by tiles.
+    #   unit.draw()
+      
     #Draw selection rectangle
     if self.selected_coords:
       s_col,s_row = self.selected_coords
@@ -184,20 +208,21 @@ class Grid:
       stroke("#FFFFCC",200)
       strokeWeight(3)
       rect(x,y,self.size,self.size)
-
-###################### Objects ########################################
+  #-----------------------------------------------------------------------------
+###################### Objects #################################################
 
 world = Grid(12,12)
-world.addUnit("Warrior",5,7)
-world.addUnit("Warrior",7,5)
-world.addUnit("Warrior",1,2)
+world.addUnit("warrior",5,7)
+world.addUnit("soldier",7,5)
+world.addUnit("boat",1,2)
 
 
-###################### Processing I/O #################################
+###################### Processing I/O ##########################################
+#-----------------------------------------------------------------------------
 def setup():
   size(520,520)
   world.draw()
-
+#-----------------------------------------------------------------------------
 def keyPressed():
   # Random-Walk over Land tiles
   if key == ' ':
@@ -207,12 +232,12 @@ def keyPressed():
       target = random.choice(land)
       unit.move(target)
   world.draw()
-
+#-----------------------------------------------------------------------------
 def mouseClicked():
   world.handleClick()
   world.draw()
-
+#-----------------------------------------------------------------------------
 def draw():
   pass
-
+#-----------------------------------------------------------------------------
 run()
