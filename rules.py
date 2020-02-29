38bf84fc670bcba01d0a68a4ef6f2fde78dc47db
HansLemurson <lemurman384@gmail.com>
2020-02-28 23:40:09 -0800

    skeleton file for planned "rules" file
    This file should be the container for all of the "Rules" data, like unit-types, terrain-types, technologies, etc.
    Currently they are attached to their relevant class, but it will be a good idea to get them all in one place.

---
+++ rules.py
@@ -0,0 +1,2 @@
+# This file should be the container for all of the "Rules" data, like unit-types, terrain-types, technologies, etc.
+# Currently they are attached to their relevant class, but it will be a good idea to get them all in one place.
